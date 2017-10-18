38970178458e6191763962b3701d0aa3363b3bd3
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-10-18 15:52:58 -0600

    validating input.
    Calling panic() if the input is invalid. Perhaps not the best way to deal with things...

--- main.go
+++ main.go
@@ -4,17 +4,40 @@ import (
   // "bytes"
   "fmt"
   "os"
+  "reflect"
+  "unicode"
   // "os/exec"
 )
+
+func isValid(s []string)(bool, string)  {
+  for _, word := range s  {
+    for _, ch := range word {
+      if !unicode.IsDigit(ch)  {
+        return false, word
+      }
+    }
+  }
+  return true, ""
+}
 
 func main()  {
   tasklist := os.Args[1:]
   fmt.Println(tasklist, tasklist[0], tasklist[len(tasklist) -1])
   fmt.Println(len(tasklist))
+  fmt.Println(reflect.TypeOf(tasklist[0]))
+
+  valid, word := isValid(tasklist)
+
+  if !valid {
+    panic(fmt.Sprintf(" Invalid input parameter: %q\n\tUse space separated numbers", word))
+  }
 
   for i := 1; i < len(tasklist); i++  {
     command := "task " + tasklist[i] + " modify depends:" + tasklist[i-1]
     fmt.Println(command)
+    for _, ch := range tasklist[i]  {
+      fmt.Printf(" %c %v\n", ch, unicode.IsDigit(ch))
+    }
   }
 
   // cmd := exec.Command("ls")
